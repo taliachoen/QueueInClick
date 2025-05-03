d2087c5cc8945203c63db8b0df54309a194f922f
taliachoen <147976038+taliachoen@users.noreply.github.com>
2025-05-04 01:04:45 +0300

    adding env page

--- Server/database/database.js
+++ Server/database/database.js
@@ -1,14 +1,11 @@
-import mysql from 'mysql2'
-//Connecting the data base to the project
-const pool = mysql.createPool(
-    {
-        host: "127.0.0.1",
-        password: "shira@123456",
-        user: 'root',
-        database: 'dbqueues'
-    }
-).promise();
+import mysql from 'mysql2';
 
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+}).promise();
 
 export default pool;
 
