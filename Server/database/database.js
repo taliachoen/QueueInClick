b7792ce5322e0691ed7519301dd4d31576699632
taliachoen <147976038+taliachoen@users.noreply.github.com>
2025-05-05 21:52:53 +0300

    check

--- Server/database/database.js
+++ Server/database/database.js
@@ -5,19 +5,22 @@ console.log("Connecting to DB with: ", {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
-  });
-  
+});
+
 import mysql from 'mysql2';
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: true
+    }
 }).promise();
 
 pool.getConnection()
-  .then(() => console.log('✅ Connected to MySQL successfully!'))
-  .catch(err => console.error('❌ Failed to connect to MySQL:', err));
+    .then(() => console.log('✅ Connected to MySQL successfully!'))
+    .catch(err => console.error('❌ Failed to connect to MySQL:', err));
 
 export default pool;
 
