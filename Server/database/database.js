96e5caf3908c38e08024bd5a7764826772e4af69
taliachoen <147976038+taliachoen@users.noreply.github.com>
2025-05-05 15:53:13 +0300

    add console

--- Server/database/database.js
+++ Server/database/database.js
@@ -1,6 +1,12 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+console.log("Connecting to DB with: ", {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+  });
+  
 import mysql from 'mysql2';
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
